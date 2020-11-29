fb38aa51744eed7f9ee0bb597a9d83da23fc97fc
Phillip Nam <pnam@Phillips-MBP.home>
2020-11-29 16:42:44 -0500

    Fixed Ensembl spelling, formatting

--- src/components/selectionDropdowns.jsx
+++ src/components/selectionDropdowns.jsx
@@ -1,49 +1,47 @@
 import React, { Component } from "react";
-import Dropdown from 'react-bootstrap/Dropdown';
+import Dropdown from "react-bootstrap/Dropdown";
 class Dropdowns extends Component {
   constructor(props) {
-    super(props)
-    console.log(props)
-    this.state={activeSpecies:'Species'};
+    super(props);
+    console.log(props);
+    this.state = { activeSpecies: "Species" };
   }
 
-  state={
-    activeSpecies:'Species'
-    
-  }
+  state = {
+    activeSpecies: "Species",
+  };
 
- handleSelect=(e)=>{
-    this.setState({activeSpecies:e})
-  }
-  
+  handleSelect = (e) => {
+    this.setState({ activeSpecies: e });
+  };
+
   render() {
     return (
-      <div className='my-4 d-flex'>
-        <Dropdown onSelect={this.handleSelect}className='m-auto' >
-          <Dropdown.Toggle variant='secondary' >
+      <div className="my-4 d-flex">
+        <Dropdown onSelect={this.handleSelect} className="m-auto">
+          <Dropdown.Toggle variant="secondary">
             {this.state.activeSpecies}
-  </Dropdown.Toggle>
+          </Dropdown.Toggle>
 
           <Dropdown.Menu>
-          {this.props.species.map((speciesName)=>(
-         <Dropdown.Item eventKey={speciesName}>{speciesName}</Dropdown.Item> ))}
-
+            {this.props.species.map((speciesName) => (
+              <Dropdown.Item eventKey={speciesName}>
+                {speciesName}
+              </Dropdown.Item>
+            ))}
           </Dropdown.Menu>
         </Dropdown>
-        <Dropdown className='m-auto'>
-          <Dropdown.Toggle variant='secondary'  >
-            Version
-  </Dropdown.Toggle>
+        <Dropdown className="m-auto">
+          <Dropdown.Toggle variant="secondary">Version</Dropdown.Toggle>
 
-          <Dropdown.Menu >
+          <Dropdown.Menu>
             <Dropdown.Item>Version 1</Dropdown.Item>
             <Dropdown.Item>Version 2</Dropdown.Item>
             <Dropdown.Item>Version 3</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </div>
-    )
-  };
-
+    );
+  }
 }
 export default Dropdowns;
