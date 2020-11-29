d2696d77636b76ad886d896ca2cdda22d7fb9eb2
Phillip Nam <pnam@Phillips-MBP.home>
2020-11-28 23:29:40 -0500

    added mySQL scripts

---
+++ mySql/scoreInsertion.sql
@@ -0,0 +1,13 @@
+SELECT * FROM mmiadDB.Score;
+
+INSERT INTO mmiadDB.Score
+VALUES
+(1,10.6094,null,null,null,null);
+
+INSERT INTO mmiadDB.Score
+VALUES
+(2,11.1105,null,null,null,null);
+
+INSERT INTO mmiadDB.Score
+VALUES
+(3,10.475,null,null,null,null);
