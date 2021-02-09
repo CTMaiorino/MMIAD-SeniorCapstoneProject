873bed5b2043a18e03fb4415375d3944dd5d46f2
James <james101544@gmail.com>
2021-02-09 11:42:03 -0500

    Add link to search results page

---
+++ client/src/searchResults.js
@@ -0,0 +1,18 @@
+import Dropdowns from "./components/selectionDropdowns";
+import SearchCriteria from "./components/searchCriteria";
+import SearchCriteriaTools from "./components/tools";
+import Header from "./components/header";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.css";
+import Form from "react-bootstrap/Form";
+import Image from "react-bootstrap/Image";
+
+
+
+function SearchResults() {
+  return (
+    <h1>Hello World!</h1>
+  )
+}
+
+export default SearchResults;
