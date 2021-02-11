ed9fa98c737b922b6ce1c79c571c4dfd0d8d3117
James <james101544@gmail.com>
2021-02-11 16:08:30 -0500

    Add fix to make express app and react work together

--- server/app.js
+++ server/app.js
@@ -16,11 +16,12 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname + '/public/build'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(
