f6bd62d5c7065eda58d24e2448226e9a97bbdc3c
Phillip Nam <pnam@Phillips-MBP.home>
2021-03-27 17:21:48 -0400

    Search route implemented (using new function)

--- server/routes/search.js
+++ server/routes/search.js
@@ -12,37 +12,132 @@ const Intron_Transcriptome_Junction = require("../models/Intron_Transcriptome_Ju
 const Intron = require("../models/Intron");
 const { Op } = require("sequelize");
 const QueryTypes = require("sequelize");
+const { text } = require("body-parser");
 const sequelize = connection.sequelize;
 
-const filteredCriteria = {}; // An object containing only non-empty search criteria values
+var filteredCriteria = {}; // An object containing only non-empty search criteria values (Global variable)
 
-// Gene.belongsToMany(Species, { foreignKey: "speciesId"});
-// Species.HasOne(Gene, { foreignKey: "speciesId"});
+// Object for debugging
+const sampleCriteria = {
+  strand: "+",
+  speciesName: "Anopheles gambiae",
+  version: "AgamP4",
+  relativeLength: 99,
+};
 
 searchRouter.use(bodyParser.json());
 
 // Step 1. This function takes in the searchCriteria object and returns a filtered object that only includes non-empty criteria values
 const filterSearchCriteria = (searchCriteria) => {
-  // Create a copy of the searchCriteria object
+  // Copy the searchCriteria object to filteredCriteria global object
   filteredCriteria = searchCriteria;
   // Loop through the copied object
   for (const key in filteredCriteria) {
-    // If value of each key/value pair is empty (''), then remove it from the object
-    if (filteredCriteria[key] == "") {
-      delete filteredCriteria.key;
+    // If value of key/value pair is empty ('') or null, then remove key from the object
+    if (filteredCriteria[key] === "" || filteredCriteria[key] === null) {
+      delete filteredCriteria[key];
     }
   }
-  console.log(filteredCritera); // (DEBUGGING) Print filteredCriteria object. Should only contain non-empty key/value pairs
+  console.log(filteredCriteria); // (DEBUGGING) Print filteredCriteria object. Should only contain non-empty key/value pairs (WORKING!)
 };
 
-// Step 2. This function takes in an array of non-empty search parameters and generates a string for a SQL query
+// Step 2. This function takes in an object of non-empty search parameters and generates a string for a SQL query
 const generateQuery = (filteredSearchParam) => {
-  // TODO: Loop through the filteredCriteria object and create a string for each key/value pair. Add them after the tableJoin string
+  var count = 1; // Helps detertime andOperator variable
+  var andOperator = ""; // Used in conjunction with counter variable for adding AND operators to whereClause
+  var whereClause = "WHERE "; // Initiate the whereClause string. It begins with a WHERE clause
+  // Loop through the filteredCriteria object and create a string for each key/value pair. Add them after the tableJoin string
+  for (const key in filteredSearchParam) {
+    var keyValue = filteredSearchParam[key];
+    if (count > 1) {
+      andOperator = "AND ";
+    }
+    switch (key) {
+      case "speciesName":
+        where = andOperator + "s.speciesName = " + "'" + keyValue + "'" + " "; // Create the WHERE statement
+        whereClause = whereClause.concat(where); // Add the where string to the whole whereClause
+        break;
+      case "version":
+        where = andOperator + "s.genomeVersion = " + "'" + keyValue + "'" + " ";
+        whereClause = whereClause.concat(where);
+        break;
+      case "ensembleGeneId":
+        where =
+          andOperator + "g.ensembleGeneId = " + "'" + keyValue + "'" + " ";
+        whereClause = whereClause.concat(where);
+        break;
+      case "ensembleTranscriptId":
+        where =
+          andOperator + "t.transcriptomeId = " + "'" + keyValue + "'" + " ";
+        whereClause = whereClause.concat(where);
+        break;
+      // Currently, there are no values for "geneSymbol" in the database
+      case "geneSymbol":
+        // where = andOperator + "s.speciesName = " + "'" + keyValue + "'" + " ";
+        // whereClause = whereClause.concat(where);
+        break;
+      case "intronClass":
+        where = andOperator + "i.intronType = " + "'" + keyValue + "'" + " ";
+        whereClause = whereClause.concat(where);
+        break;
+      case "exactLength":
+        where = andOperator + "i.intronLength = " + "'" + keyValue + "'" + " ";
+        whereClause = whereClause.concat(where);
+        break;
+      case "relativeLength":
+        where = andOperator + "g.geneLength = " + "'" + keyValue + "'" + " ";
+        whereClause = whereClause.concat(where);
+        break;
+      case "strand":
+        where = andOperator + "i.strand = " + "'" + keyValue + "'" + " ";
+        whereClause = whereClause.concat(where);
+        break;
+      // Currently, there are no values for "coordinates" in the database
+      case "coordinates":
+        // where = andOperator + "s.species = " + "'" + keyValue + "'" + " ";
+        // whereClause = whereClause.concat(where);
+        break;
+      // Currently, there are no values for "relativeExonLength" in the database
+      case "relativeExonLength":
+        // where = andOperator + "s.species = " + "'" + keyValue + "'" + " ";
+        // whereClause = whereClause.concat(where);
+        break;
+      case "sequence":
+        where =
+          andOperator +
+          "WHERE i.intronSequence = " +
+          "'" +
+          keyValue +
+          "'" +
+          " ";
+        whereClause = whereClause.concat(where);
+        break;
+      default:
+        break;
+    }
+    count++;
+  }
   const tableJoin =
-    "SELECT * FROM Species s INNER JOIN Gene g ON s.speciesId = g.speciesId INNER JOIN Transcriptome t ON g.geneNumId = t.geneNumId INNER JOIN intron_transcriptome_junction itj ON t.transcriptomeNumId = itj.transcriptomeNumId INNER JOIN Intron i ON itj.intronNumId = i.intronNumId INNER JOIN Exon e ON i.intronNumId = e.intronNumId INNER JOIN intron_score_junction isj ON i.intronNumId = isj.intronNumId INNER JOIN Score score ON isj.scoreId = score.scoreId";
-  const generatedQuery = tableJoin; /*+*/ // The final SQL query string after adding tableJoin and extra WHERE clauses together
-  console.log;
+    "SELECT * FROM Species s INNER JOIN Gene g ON s.speciesId = g.speciesId INNER JOIN Transcriptome t ON g.geneNumId = t.geneNumId INNER JOIN intron_transcriptome_junction itj ON t.transcriptomeNumId = itj.transcriptomeNumId INNER JOIN Intron i ON itj.intronNumId = i.intronNumId INNER JOIN Exon e ON i.intronNumId = e.intronNumId INNER JOIN intron_score_junction isj ON i.intronNumId = isj.intronNumId INNER JOIN Score score ON isj.scoreId = score.scoreId ";
+  var generatedQuery = tableJoin + whereClause; // The final SQL Query statement. Result of tableJoin + whereClause
+  console.log("PRINTING whereClause PORTION: " + whereClause); // (DEBUGGING) Check the whereClause portion of the entire query
+  console.log("PRINTING GENERATED QUERY: " + generatedQuery); // (DEBUGGING) Check the final SQL statement
+  return generatedQuery;
 };
+
+searchRouter.post("/main", async (req, res) => {
+  filterSearchCriteria(req.body);
+  const sqlQuery = generateQuery(filteredCriteria);
+  return sequelize
+    .query(sqlQuery, {
+      type: QueryTypes.SELECT,
+    })
+    .then((response) => {
+      console.log(response);
+      res.json(response);
+      return response;
+    });
+});
 
 /*****TEST CODE*****/
 
@@ -245,6 +340,7 @@ searchRouter.get("/Intron/:subtype", function (req, res) {
 */
 
 /* GET search listings. */
+/*
 searchRouter.get("/", function (req, res, next) {
   connection.query(
     "SELECT * FROM `Species` ",
@@ -254,6 +350,7 @@ searchRouter.get("/", function (req, res, next) {
     }
   );
 });
+*/
 
 // GET all species
 searchRouter.get("/species", function (req, res, next) {
