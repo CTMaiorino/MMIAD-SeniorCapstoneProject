d84293dd2db043f720b6ffba4db28814a4cf3987
CTMaiorino <32141103+CTMaiorino@users.noreply.github.com>
2021-04-13 21:11:41 -0400

    added GTF file export function

--- client/src/exportHelperFunctions.js
+++ client/src/exportHelperFunctions.js
@@ -8,9 +8,27 @@ You will also need to change the import in exportButtons.jsx to match the functi
 
 
 export const exportAsGtf = (introns) => {
+    var geneString = ""
+    var transcriptString = ""
     var totalString = ""
     introns.forEach((intron) => {
-
+        //Here, it grabs information about the intron that is needed for the file. You will need to do this as well for different properties
+        const sequence = intron.sequence
+        const geneId = intron.ensemblGeneId
+        const start = intron.intronStartCoord
+        const end = intron.intronStartCoord
+        const score = intron.overallScore
+        const strand = intron.strand
+        const transcriptomeId = intron.transcriptomeId
+        //Following data points do not currently exist in the DB
+        //Replace at later date when data is supported 
+        const geneSource = "N/A"
+        const geneName = "N/A" // intron.geneName is currently null in DB
+        const geneBioType = "transcribed_unprocessed_pseudogene"
+        //GTF format has these properties separated by tabs, followed by a new line
+        geneString = "transcribed_unprocessed_pseudogene  gene " + "   " + "gene_id: " + geneId + "   " + "gene_name: " + geneName + "   " + "gene_source: " + geneSource + "   " + "gene_start: " + start + "   " + "gene_end: " + end + "   " + "gene_score: " + score + "   " + "gene_strand: " + strand + "   " + "gene_sequence: " + sequence + "   " + "gene_bioType: " + geneBioType + "\n"
+        transcriptString = "processed_transcript                transcript " + "   " + "gene_id: " + geneId + "   " + "transcript_id: " + transcriptomeId + "   " + "gene_name: " +geneName + "   " + "gene_source: " + geneSource + "   " + "gene_bioType: " + geneBioType + "   " + "gene_name: " +geneName + "\n"
+        totalString += geneString + transcriptString + "\n"
     })
     downloadFile(totalString, "gtf");
 }
